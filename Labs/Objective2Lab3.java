a93d4b8b1a0336056d792d1c675c667018dd96b6
DylanBeppler <dylanbeppler@gmail.com>
2023-11-05 00:11:34 -0500

    complete objective2lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,10 @@
+public class Objective2Lab3 {
+	public static void main(String[] args) {
+		int birthday = 1993;
+		String month = "June";
+		boolean weekend = false;
+		System.out.println(birthday);
+	 	System.out.println(month);
+	 	System.out.println(weekend);
+	 }
+}
