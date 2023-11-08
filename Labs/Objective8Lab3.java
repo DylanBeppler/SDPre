e5269d80b68168ffac27649de9d607a3b4f754c2
DylanBeppler <dylanbeppler@gmail.com>
2023-11-08 14:51:49 -0600

    complete objective8lab3

---
+++ Labs/Objective8Lab3.java
@@ -0,0 +1,15 @@
+public class Objective8Lab3 {
+    public static void main(String[] args) {
+        
+
+     for (int counter = 1; counter <= 20; counter++) {
+            System.out.print(counter);
+
+            if (counter % 2 == 0) {
+                System.out.println(" is even");
+            } else {
+                System.out.println(" is odd");
+            }
+        }
+    }
+}
